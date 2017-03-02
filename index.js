5de6cca35edb9ee7d314b43b3270dba44efea4a3
Chad Gibson <gibson.coding@gmail.com>
2017-03-02 14:36:23 -0500

    Fixed get bug

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5501;
 
-app.get('/test', function(req, res) {
+app.get('/', function(req, res) {
     res.json({message: 'Heroku, world!'});
 });
 
-app.get('/test/:name', function(req, res) {
-
+app.get('/:name', function(req, res) {
     var name = req.params.name;
-    
     res.json({message: 'Heroku, ' + name})
 });
 
-
 app.listen(port, function(err) {
-
+    if (err) {
+        console.log(err);
+    };
 });
